57b9518035678abe83dc1595c1b269a70f839589
Anna Zhukovskaya <avzhukovskaya@gmail.com>
2024-12-12 17:32:14 +0300

    App without jsx

--- src/App.js
+++ src/App.js
@@ -3,26 +3,38 @@ import './App.css';
 import { Year } from './components/Year';
 import React from 'react';
 
-//В данном случае использован - декларативный подход. В данном случае это выражается через JSX. Четко описывается, что нужно сделать, а не как.
-
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<Year />
-			</header>
-		</div>
+	return React.createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		React.createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement(
+				'p',
+				null,
+				'Edit <code>src/App.js</code> and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement(Year, null),
+		),
 	);
 };
